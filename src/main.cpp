8f21d2f176b8ef7f4b62e938763bfaeb579eb915
Aki Zhou <aki@akizhou.com>
2022-03-22 07:09:42 +0900

    Add demo main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
 #include "Arduino.h"
 
-void setup() {}
+#define FSR_PIN A7
+#define BAUD_RATE 9600
 
-void loop() {}
+int fsr_reading;
+float vo;
+float fsr;
+float force;
+
+void setup() { Serial.begin(BAUD_RATE); }
+
+void loop() {
+  fsr_reading = analogRead(FSR_PIN);
+  // TODO: Fix mapping to reflect the logarithmic change
+  vo = map(fsr_reading, 0, 1023, 0, 5000);
+  fsr = (500000 - vo * 100000) / vo;
+  force = (1 / fsr) * 1000.0;
+
+  Serial.println(vo);
+  // Serial.println(vo);
+
+  // Serial.print("Force read: ");
+  // Serial.print(force);
+  // Serial.println(" g");
+
+  delay(100);
+}
